c0787f1768874ce7ba722ce4d108b9f5bc36ad81
Akshat Ramanathan <66016691+Aksh247@users.noreply.github.com>
2021-03-15 19:20:02 +0530

    15-03-21
    PROJECT FILES

---
+++ config.py
@@ -0,0 +1,21 @@
+
+
+# --- CONFIGURATIONS FOR Create_Dataset_as_csv --- #
+class CreateDataset:
+    # Path of GTZAN dataset
+    DATASET_DIRECTORY = "./TrainFiles/"
+
+    # Sampling rate (Hz)
+    SAMPLING_RATE = 22050
+
+    # Frame size (Samples)
+    FRAME_SIZE = 2048
+
+    # Hop Size (Samples)
+    HOP_SIZE = 512
+
+
+class Test:
+    # Path for test data
+    TEST_DATA_PATH = "./TestFiles/"
+
